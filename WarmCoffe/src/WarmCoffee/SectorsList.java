30e93ff7e6552cad33f41a264a02017ed219a3d9
rafaeling <rafaelingugr@gmail.com>
2015-12-29 13:30:09 +0100

    New Class

---
+++ WarmCoffe/src/WarmCoffee/SectorsList.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WarmCoffee;
+
+import GUI.SectorsListView;
+
+/**
+ *
+ * @author rafae
+ */
+public class SectorsList extends SectorsListView{
+    
+    public SectorsList()
+    {
+        super();
+    }
+    
+}
